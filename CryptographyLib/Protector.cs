2538f3d48da495005dac56c07cf1b49503c3d487
Олег Бабков <babkoff1989@gmail.com>
2019-08-07 18:26:50 +0300

    encription added

--- CryptographyLib/Protector.cs
+++ CryptographyLib/Protector.cs
@@ -1,8 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Linq;
 
 namespace CryptographyLib
 {
     public class Protector
     {
+        // Размер должен составлять не менее 8 байт,
+        // мы будем использовать 16 байт
+        private static readonly byte[] salt = Encoding.Unicode.GetBytes("7BANANAS");
+
+        // Число итераций должно быть не меньше 1000, мы будем использовать
+        // 2000 итераций
+        private static readonly int iterations = 2000;
+
+        public static string Encrypt(string plainText, string password)
+        {
+            byte[] plainBytes = Encoding.Unicode.GetBytes(plainText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            aes.Key = pbkdf2.GetBytes(32); // установить 256-битный ключ
+            aes.IV = pbkdf2.GetBytes(16);  // установить 128-битный вектор инициализации
+            var ms = new MemoryStream();
+            using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                cs.Write(plainBytes, 0, plainBytes.Length);
+            }
+            return Convert.ToBase64String(ms.ToArray());
+        }
+
+        public static string Decrypt(string cryptoText, string password)
+        {
+            byte[] cryptoBytes = Convert.FromBase64String(cryptoText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            aes.Key = pbkdf2.GetBytes(32);
+            aes.IV = pbkdf2.GetBytes(16);
+            var ms = new MemoryStream();
+            using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+            {
+                cs.Write(cryptoBytes, 0, cryptoBytes.Length);
+            }
+            return Encoding.Unicode.GetString(ms.ToArray());
+        }
+
     }
 }
---
+++ EncryptionApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CryptographyLib;
+using static System.Console;
+
+namespace EncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter a message that you want to encrypt: ");
+            string message = ReadLine();
+            Write("Enter a password: ");
+            string password = ReadLine();
+            string cryptoText = Protector.Encrypt(message, password);
+            WriteLine($"Encrypted text is {cryptoText}");
+            Write("Enter a the password: ");
+            string password2 = ReadLine();
+            try
+            {
+                string clearText = Protector.Decrypt(cryptoText, password2);
+                WriteLine($"Decrypted text : {clearText}");
+            }
+            catch
+            {
+                WriteLine("Disable to decrypt because you entered the wrong password!");
+                throw;
+            }
+        }
+    }
+}
